2cd26317ec55da1216d98fa01008c04e6aa51b8c
magdalan01 <61440670+magdalan01@users.noreply.github.com>
2020-02-26 10:31:00 +1100

    initial comment

---
+++ main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    float x;
+    float y;
+    printf("Enter a number: ");
+    scanf("%f", &x);
+    y = 9*x + 32;
+    printf("y: %f\n", y);
+}
